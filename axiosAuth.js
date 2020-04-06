0565fcabdf71afadc9662cf1e0d4e16401b5f2a3
Marcos Lopes <marcos@dourasoft.com.br>
2020-04-06 12:02:13 -0400

    Consumindo api o painel administrativo;

--- axiosAuth.js
+++ axiosAuth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let token = localStorage.getItem('token')
+let token = localStorage.getItem('token');
 
 const instance = axios.create({
   baseURL:'http://api-finac.herokuapp.com',
